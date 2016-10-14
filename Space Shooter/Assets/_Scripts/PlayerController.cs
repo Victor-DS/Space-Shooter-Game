796017a6a4406f26ab8a956e83b1b659d607fa66
Victor D. S <vdsantiago.1@gmail.com>
2016-10-14 15:45:07 -0300

    Shooting added!

--- Space Shooter/Assets/_Scripts/PlayerController.cs
+++ Space Shooter/Assets/_Scripts/PlayerController.cs
@@ -5,7 +5,13 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public float tilt;
+	public float fireRate = 0.25f;
+	private float nextFire = 0.0f;
+
 	public Boundary boundary;
+	
+	public GameObject shot;
+	public Transform shotSpawn;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +20,10 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
 	}
 
 	// FixedUpdate should be used instead of Update when dealing with Rigidbody.
